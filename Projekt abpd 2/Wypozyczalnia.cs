f2609404d3e4b83739819f0325d78124bc03e5f9
Michał Biel <s30491@pjwstk.edu.pl>
2026-03-22 11:55:50 +0100

    Add wypożyczalnia

---
+++ Projekt abpd 2/Wypozyczalnia.cs
@@ -0,0 +1,18 @@
+﻿using Projekt_abpd_2.modele;
+
+namespace Projekt_abpd_2;
+
+public class Wypozyczalnia
+{
+    private readonly List<Sprzet> _ekwipunek = new();
+    private readonly List<Uzytkownik> _uzytkownicy = new();
+    private readonly List<Wypozyczenie> _wypozyczenia = new();
+
+    private const decimal KaraZaDzien = 10;
+
+    public void DodajSprzet(Sprzet sprzet) => _ekwipunek.Add(sprzet);
+    public void DodajUzytkownika(Uzytkownik uzytkownik) => _uzytkownicy.Add(uzytkownik);
+
+    
+
+}
