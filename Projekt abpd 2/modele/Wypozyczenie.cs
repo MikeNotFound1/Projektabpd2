3f42daf9fde97b5d0246b0374a3c127c7f568b61
Michał Biel <s30491@pjwstk.edu.pl>
2026-03-22 11:54:42 +0100

    Add wypożycznie

---
+++ Projekt abpd 2/modele/Wypozyczenie.cs
@@ -0,0 +1,12 @@
+﻿namespace Projekt_abpd_2.modele;
+
+public class Wypozyczenie
+{
+    public Uzytkownik Uzytkownik { get; set; }
+    public Sprzet Sprzet { get; set; }
+    public DateTime DataWypozyczenia { get; set; }
+    public DateTime TerminZwrotu { get; set; }
+    public DateTime? DataZwrotu { get; set; }
+    public decimal Kara { get; set; } = 0;
+    public bool PoTerminie => DataZwrotu.HasValue ? DataZwrotu > TerminZwrotu : DateTime.Now > TerminZwrotu;
+}
